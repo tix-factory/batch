bb199d8fc6f64b3ea1eceb5d2cf01448f29e3b59
WebGL3D <WebGL3D@users.noreply.github.com>
2023-04-09 10:09:12 -0700

    Begin implementation

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// Export all the things from this module.
+export { default as Batch } from './batch';
+export { default as ErrorEvent } from './errorEvent';
+
+// Types
+export { default as BatchItem } from './types/batchItem';
+export { default as BatchConfiguration } from './types/batchConfiguration';
