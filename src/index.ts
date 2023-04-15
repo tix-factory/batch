eb3dbc898a5fc027f90d68f36f4a8620347aac55
WebGL3D <WebGL3D@users.noreply.github.com>
2023-04-15 10:58:42 -0700

    Revert "Begin implementation"
    This reverts commit bb199d8fc6f64b3ea1eceb5d2cf01448f29e3b59.

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-// Export all the things from this module.
-export { default as Batch } from './batch';
-export { default as ErrorEvent } from './errorEvent';
-
-// Types
-export { default as BatchItem } from './types/batchItem';
-export { default as BatchConfiguration } from './types/batchConfiguration';
